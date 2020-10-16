90702a9e11d65692d10d07fe9e78b1324e13c22a
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-15 22:13:11 -0700

    fix: missing users json

--- modules/user.py
+++ modules/user.py
@@ -1,7 +1,11 @@
-import json
+""" import json
 
 with open("users.json") as file:
-    users = json.load(file)
+    users = json.load(file) """
+
+users = {
+    "narendrasss": "b7a8d519-63fb-4cbb-bb68-b37360b6d70b"
+}
 
 
 def get_user(client, login):

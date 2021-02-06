dc2c061b4236edb71b4dc2ae0a08682a262b4450
Narendra Syahrasyad <nanda.s@hey.com>
2021-02-05 17:50:38 -0800

    feat: add feedback endpoint

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 
 from modules.action import Action, parse
 from modules.util import validate_signature
+from modules.client import board
 import modules.tickets as tickets
 
 app = Flask(__name__)
@@ -26,3 +27,13 @@ def main():
         tickets.update_ticket(ticket_number, action)
 
     return ("", 204)
+
+
+@app.route('/feedback', methods=['POST'])
+def addFeedback():
+    feedback = board.collection.add_row()
+    body = request.json
+    feedback.title = body["name"]
+    feedback.message = body["message"]
+
+    return ("", 201)
